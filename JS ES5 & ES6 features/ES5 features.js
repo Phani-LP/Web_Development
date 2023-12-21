53b904303f5e6d7ec3b2e4073dfc86a8bcaa6426
Phani-LP <dnagaphanindrait@gmail.com>
2023-12-21 21:13:06 +0530

    New content added

--- JS ES5 & ES6 features/ES5 features.js
+++ JS ES5 & ES6 features/ES5 features.js
@@ -1,3 +1,17 @@
-//Examples:
-var arr = [3,5,4,8,15]
-var output = 
+//Examples: For ES5 features
+var myArray = [-33,5,4,8,15];
+console.log(Array.isArray(myArray));
+
+var output1 = myArray.map(
+    function(element){
+        return (element + 20);
+    }
+)
+console.log(output1)
+
+let output2 = myArray.filter(
+    function (element){
+        return element > 2
+    }
+)
+console.log(output2)
